90fa4ae428c6e8bb35f866ce05f28feb1f0ebfb4
Shakur Chowdhury <33189134+chowdhuryshakur@users.noreply.github.com>
2019-10-06 10:10:49 +0600

    Delete VenueRepository.java

--- manage-meetin/src/main/java/com/managemeeting/managemeetin/venue/VenueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.managemeeting.managemeetin.venue;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VenueRepository extends MongoRepository<Venue,String> {
-}
