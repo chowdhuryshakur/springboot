ef253b5a2fdc63210b1febf9a2803187e1447a15
Shakur Chowdhury <33189134+chowdhuryshakur@users.noreply.github.com>
2019-10-06 10:11:17 +0600

    Delete MemberRepository.java

--- manage-meetin/src/main/java/com/managemeeting/managemeetin/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.managemeeting.managemeetin.member;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends MongoRepository<Member,String> {
-
-}
