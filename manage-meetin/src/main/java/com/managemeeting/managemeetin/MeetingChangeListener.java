7466028d632afcf0b8b27129dfd5e38f1a1435e4
Shakur Chowdhury <33189134+chowdhuryshakur@users.noreply.github.com>
2019-10-06 10:09:56 +0600

    Delete MeetingChangeListener.java

--- manage-meetin/src/main/java/com/managemeeting/managemeetin/MeetingChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.managemeeting.managemeetin;
-
-import com.managemeeting.managemeetin.meeting.Meetings;
-
-public interface MeetingChangeListener {
-    void meetingChanged(Meetings meeting);
-}
